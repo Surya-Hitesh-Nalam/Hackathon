bdbd26c220ea148d94479c8948d79f5fb530ec4f
Surya-Hitesh-Nalam <167790489+Surya-Hitesh-Nalam@users.noreply.github.com>
2024-12-14 09:10:11 +0530

    Update MainLayout.jsx

--- client/src/layout/MainLayout.jsx
+++ client/src/layout/MainLayout.jsx
@@ -1,14 +1,8 @@
 import LandingPage from '@/pages/LandingPages/LandingPage'
-import { Outlet } from 'react-router-dom'
 
 const MainLayout = () => {
-  return (
-    <div className='flex flex-col min-h-screen'>
+  return ( 
       <LandingPage />
-      <div className='flex-1 mt-16'>
-        <Outlet />
-      </div>
-    </div>
   )
 }
 
