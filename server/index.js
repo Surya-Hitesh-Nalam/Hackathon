92c068baeffd6aa140ed624afad2b01c4f9a627e
Surya-Hitesh-Nalam <167790489+Surya-Hitesh-Nalam@users.noreply.github.com>
2024-12-14 09:25:33 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://hackathon-br2x.onrender.com",
     credentials:true
 }));
  
